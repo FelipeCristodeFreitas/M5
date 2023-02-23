3a079279ac2413c82d07b078a194c1be4cd72984
FelipeCristo <felipe.cristo@yahoo.com.br>
2023-02-22 21:12:49 -0300

    vercel

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { get } from 'http';
+import { createWriteStream } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,13 +12,56 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('XboxLive')
-    .setDescription('Aplicação para GAMES')
+    .setDescription('AplicaÃ§Ã£o para GAMES')
     .setVersion('1.0.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
 
   await app.listen(3333);
+
+  // write swagger ui files
+  get(`/api/swagger-ui-bundle.js`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+    console.log(
+      `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+    );
+  });
+
+  get(`/api/swagger-ui-init.js`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    console.log(
+      `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+    );
+  });
+
+  get(`/api/swagger-ui-standalone-preset.js`, function (response) {
+    response.pipe(
+      createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+    );
+    console.log(
+      `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+    );
+  });
+
+  get(`/api/swagger-ui.css`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    console.log(
+      `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+    );
+  });
 }
 bootstrap();
